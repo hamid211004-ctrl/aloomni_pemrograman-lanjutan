f689aef043b4062163dc506f57d7c9d32c825ab3
rohmanhamit <hamid211004@gmail.com>
2026-06-26 00:43:28 +0700

    desain_aplikasi aloomni dan mengaktifkan sidebarnya supaya bisa pindah panel

---
+++ src/alumni_202557201020/Alumni_202557201020.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package alumni_202557201020;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Alumni_202557201020 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
